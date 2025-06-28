98a0e8f46aa6cc9f6227ac1d0457678c01a77f58
masolites <mfmmpcs@gmail.com>
2025-06-28 23:34:10 +0100

    Update firebase.js

--- contracts/firebase/firebase.js
+++ contracts/firebase/firebase.js
@@ -1,11 +1,17 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAo1BHpTKsFSsqBgZLARkyNWslGBEt7aE4",
   authDomain: "paze-b529b.firebaseapp.com",
-  databaseURL: "https://paze-b529b-default-rtdb.firebaseio.com",
   projectId: "paze-b529b",
   storageBucket: "paze-b529b.appspot.com",
   messagingSenderId: "838658270171",
-  appId: "1:838658270171:web:60138628a6edbbf7cb7dfe",
-  measurementId: "G-L2SN492BWK"
+  appId: "1:838658270171:web:60138628a6edbbf7cb7dfe"
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
