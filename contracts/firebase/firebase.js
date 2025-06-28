95988fd0bc7b526ea41346eb44a5545c5272f34f
masolites <mfmmpcs@gmail.com>
2025-06-28 23:20:11 +0100

    Create firebase.js

---
+++ contracts/firebase/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAo1BHpTKsFSsqBgZLARkyNWslGBEt7aE4",
+  authDomain: "paze-b529b.firebaseapp.com",
+  databaseURL: "https://paze-b529b-default-rtdb.firebaseio.com",
+  projectId: "paze-b529b",
+  storageBucket: "paze-b529b.appspot.com",
+  messagingSenderId: "838658270171",
+  appId: "1:838658270171:web:60138628a6edbbf7cb7dfe",
+  measurementId: "G-L2SN492BWK"
+};
